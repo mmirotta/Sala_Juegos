7c37db26b61962c97402e83dbf6bfd4f6c021e9c
matias mirotta <m.mirotta@gmail.com>
2016-09-04 10:39:58 -0300

    Primer commit. Se sube todo el proyecto para comenar a realizar los cambios.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+var app = angular.module('salaJuegos', []);
+
+app.controller('controlLogin', function($scope) {
+
+  $scope.usuarioLogeado = "";
+
+});
+
+app.controller('controlAlta', function($scope) {
+
+
+});
+
